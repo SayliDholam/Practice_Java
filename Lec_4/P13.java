8604063f694d628bed0f8a2092706db36cf6997a
SayliDholam <saylidholam08@gmail.com>
2024-03-09 20:38:43 +0530

    Pattern no. 13

---
+++ Lec_4/P13.java
@@ -0,0 +1,22 @@
+/*
+A       A       A       A
+B       B       B       B
+C       C       C       C
+D       D       D       D
+*/
+
+class P13{
+	public static void main(String[] args) {
+		int i, j, n=65;
+		for (i = 1; i<= 4 ;i++ ) {
+			for (j = 1; j <= 4; j++ ) {
+				System.out.print((char)n + "\t");
+			
+			}
+			n++;
+			System.out.println();
+			
+		}
+	}
+	
+}
