fa4ae77f147cb80926b40125e49ab95fd8e04752
SayliDholam <saylidholam08@gmail.com>
2024-03-09 21:08:59 +0530

    Continue

---
+++ Lec_4/Continue.java
@@ -0,0 +1,13 @@
+//continue
+
+class Continue{
+	public static void main(String[] args) {
+		for(int i = 1; i<=10; i++){
+			if(i == 5)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
