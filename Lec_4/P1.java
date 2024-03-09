63eb0285a63017001c6f857e89d781f876ee290f
SayliDholam <saylidholam08@gmail.com>
2024-03-09 21:07:50 +0530

    Pattern no. 1

---
+++ Lec_4/P1.java
@@ -0,0 +1,18 @@
+/*pattern1
+1111
+2222
+3333
+4444
+*/
+class P1{
+	public static void main(String[] args) {
+		int i, j;
+		for (i = 1; i<= 4 ;i++ ) {
+			for (j = 1; j <= 4; j++ ) {
+				System.out.print(i);
+			}
+			System.out.println();
+			
+		}
+	}
+}
