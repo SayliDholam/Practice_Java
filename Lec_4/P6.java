07f6986a381f6cabd4675317a3a967f442709e36
SayliDholam <saylidholam08@gmail.com>
2024-03-09 21:02:28 +0530

    Pattern no. 6

---
+++ Lec_4/P6.java
@@ -0,0 +1,23 @@
+/*pattern6
+   *
+  **
+ ***
+****
+*/
+class P6{
+	public static void main(String[] args) {
+		int i, j, k, sp = 3 ;
+		for (i = 1; i<=4 ; i++) {
+			for (k = 1; k<=sp; k++) {
+				System.out.print(" ");
+			}
+			for (j = 1; j<=i ; j++ ) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+			sp--;
+			
+		}
+	}
+}
