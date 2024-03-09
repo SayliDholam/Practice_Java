0516bb9031d7a6bb824080e03e9f83978fa2f7cf
SayliDholam <saylidholam08@gmail.com>
2024-03-09 21:06:02 +0530

    Pattern no. 3

---
+++ Lec_4/P3.java
@@ -0,0 +1,18 @@
+/*pattern3
+****
+***
+**
+*
+*/
+class P3{
+	public static void main(String[] args) {
+		int i, j;
+		for (i = 4; i>=1 ;i-- ) {
+			for (j = 1; j <= i; j++ ) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+}
