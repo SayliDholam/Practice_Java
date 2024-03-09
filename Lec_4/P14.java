62ff0226beeb24d363c83abd24782df22cd2946c
SayliDholam <saylidholam08@gmail.com>
2024-03-09 20:31:35 +0530

    Pattern no. 14

---
+++ Lec_4/P14.java
@@ -0,0 +1,23 @@
+/*
+A       B       C       D       E
+A       B       C       D       E
+A       B       C       D       E
+A       B       C       D       E
+*/
+
+class P14{
+	public static void main(String[] args) {
+		int i, j, n=65;
+		for (i = 1; i<= 4 ;i++ ) {
+			for (j = 65; j <= 69; j++ ) {
+				System.out.print((char)j + "\t");
+
+			
+			}
+	
+			System.out.println();
+			
+		}
+	}
+	
+}
