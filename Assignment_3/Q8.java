39833c0a27c82a2f23aa5049cc2ae4430fede746
SayliDholam <saylidholam08@gmail.com>
2024-03-10 14:15:55 +0530

    pattern no. 8

---
+++ Assignment_3/Q8.java
@@ -0,0 +1,41 @@
+/*
+pattern :
+    J I H G F E D C B A
+    J I H G F E D C B
+    J I H G F E D C
+    J I H G F E D
+    J I H G F E
+    J I H G F
+    J I H G
+    J I H
+    J I
+    J
+
+*/
+
+class Q8
+{
+	public static void main(String[] args) 
+	{
+		int i, j ;
+		for (i = 1; i <= 10; i++ ) 
+		{
+			for (j = 10; j >= i; j--) 
+			{
+				System.out.print((char)('A' - 1 + j) + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+/*
+
+('A' - 1 + j): This expression calculates the ASCII value of the character to be printed in each iteration.
+
+''A' is the starting character.
+-1 is subtracted to adjust the position because we want to start from 'J' and go backwards.
++j is added to move through the characters in the reverse order.
+
+*/
