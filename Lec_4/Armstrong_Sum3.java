0c5932806558ef567b4117558856d9795f3b1c48
SayliDholam <saylidholam08@gmail.com>
2024-03-10 14:31:14 +0530

    To find sum of 3 digits armstrong numbers

--- Lec_4/Armstrong_Sum3.java
+++ Lec_4/Armstrong_Sum3.java
@@ -1,33 +1,38 @@
-class Armstrong_Sum3 {
-    public static void main(String[] args) {
+class Armstrong_Sum3 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("Armstrong numbers upto 1000 are : ");
 
-        double sum1 = 0; // Declare and initialize sum1 outside the loop
+        double sum1 = 0; 
 
-        for (int i = 0; i <= 1000; i++) {
+        for (int i = 0; i <= 1000; i++) 
+        {
             int num = i;
             int count = 0;
             double sum = 0;
             int temp = num;
 
-            while (temp > 0) {
+            while (temp > 0) 
+            {
                 temp = temp / 10;
                 count++;
             }
             temp = num;
 
-            while (temp > 0) {
+            while (temp > 0) 
+            {
                 int rem = temp % 10;
                 sum = sum + Math.pow(rem, count);
                 temp = temp / 10;
             }
 
-            if (num == sum && count == 3) {
+            if (num == sum && count == 3) 
+            {
                 System.out.println(num);
                 sum1 = sum1 + sum;
             }
         }
-
         System.out.println("Sum of 3 digits armstrong is : " + sum1);
     }
 }
