0a390aeb47178f70c07bb38a690a0f1cb0d03e29
SayliDholam <saylidholam08@gmail.com>
2024-03-10 12:18:09 +0530

    pattern no. 6

---
+++ Assignment_3/Q6.java
@@ -0,0 +1,40 @@
+/*
+pattern :
+    J I H G F E D C B A
+    J I H G F E D C B A
+    J I H G F E D C B A
+    J I H G F E D C B A
+    J I H G F E D C B A
+    J I H G F E D C B A
+    J I H G F E D C B A
+    J I H G F E D C B A
+    J I H G F E D C B A
+    J I H G F E D C B A   
+*/
+
+class Q6{
+	public static void main(String[] args) {
+		int i, j  ;
+		for (i = 1; i <= 10; i++ ) {
+			for ( j = 10 ; j >= 1; j--) {
+				System.out.print((char)('A' - 1 + j) + " ");
+				
+			}
+			System.out.println();
+			
+		}
+
+	}
+}
+
+
+
+/*
+
+('J' - 1 + j): This expression calculates the ASCII value of the character to be printed in each iteration.
+
+''J' is the starting character.
+-1 is subtracted to adjust the position because we want to start from 'J' and go backwards.
++j is added to move through the characters in the reverse order.
+
+*/
