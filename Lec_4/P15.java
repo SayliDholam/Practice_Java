87d8a40ad797fef563f690fb6b4dc6a21aa377ea
SayliDholam <saylidholam08@gmail.com>
2024-03-10 11:01:06 +0530

    Pattern no. 15

--- Lec_4/P15.java
+++ Lec_4/P15.java
@@ -5,16 +5,18 @@
 2       2       2       2       2
 1       1       1       1       1
 */
-class P15{
-	public static void main(String[] args) {
+class P15
+{
+	public static void main(String[] args) 
+	{
 		int i, j;
-		for (i = 5; i>= 1 ;i-- ) {
-			for (j = 1; j <= 5; j++ ) {
+		for (i = 5; i>= 1 ;i-- ) 
+		{
+			for (j = 1; j <= 5; j++ ) 
+			{
 				System.out.print(i + "\t");
-			
 			}
 			System.out.println();
-			
 		}
 	}
 }
