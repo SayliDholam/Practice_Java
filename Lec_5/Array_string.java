e2c8d961e7cb79d141d83dc4495471d5bfd4194b
SayliDholam <saylidholam08@gmail.com>
2024-03-23 20:09:49 +0530

    1D array in string

---
+++ Lec_5/Array_string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Array_string
+{
+	public static void main(String[] args) {
+
+		int arr[] = new int[]{5,7,43,87,34,23,956,3,343};
+
+		System.out.println(Arrays.toString(arr));
+	}
+}
+
