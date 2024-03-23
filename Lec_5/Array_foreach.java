fb285b86dde5774d16d78b5e265e85d218ddcb33
SayliDholam <saylidholam08@gmail.com>
2024-03-23 20:17:02 +0530

    1D array using foreach loop

---
+++ Lec_5/Array_foreach.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Array_foreach
+{
+	public static void main(String[] args) {
+
+		int arr[] = new int[]{5,7,43,87,34,23,956,3,343};
+
+		for(int i : arr)
+		{
+			System.out.print(" " + i);
+		}
+	}
+}
+
