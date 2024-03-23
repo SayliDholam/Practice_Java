5cb27e03dc9c9e42fbd634c37d8fc30430c907d2
SayliDholam <saylidholam08@gmail.com>
2024-03-23 19:57:49 +0530

    Sum of even elements

---
+++ Lec_5/ArraySumEven.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class ArraySumEven
+{
+	public static void main(String[] args) {
+
+		int arr[] = new int[5];
+		int sum = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter array elements : ");
+
+		for(int i = 0; i < 5; i++){
+			arr[i] = sc.nextInt();
+		}
+
+
+		for(int i = 0; i < arr.length ; i++){
+			if(arr[i] % 2 == 0 )
+			{
+				sum = sum + arr[i];
+			}
+		}
+		System.out.println("Sum of the even Array elements are : " + sum);
+
+	}
+}
