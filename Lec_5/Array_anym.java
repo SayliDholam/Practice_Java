40b93a2265c3bb24dd225f5cfc0a375953530719
SayliDholam <saylidholam08@gmail.com>
2024-03-23 20:54:00 +0530

    Anonymous 1D array

---
+++ Lec_5/Array_anym.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Array_anym
+{
+	public static void main(String[] args) {
+
+		System.out.println(new int[]{46, 85, 35, 84, 22}.length);
+		System.out.println(new int[]{86,45,2,532,25}[3]);
+	}
+}
+
