22b17fa93e046c5c13dc13d0ea901d1ff9e27ae0
SayliDholam <saylidholam08@gmail.com>
2024-03-23 20:12:52 +0530

    1D array sorting

---
+++ Lec_5/Array_sort.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Array_sort
+{
+	public static void main(String[] args) {
+
+		int arr[] = new int[]{5,7,43,87,34,23,956,3,343};
+
+		Arrays.sort(arr);
+
+		System.out.println(Arrays.toString(arr));
+	}
+}
+
