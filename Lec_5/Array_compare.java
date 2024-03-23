c29fb58eab2e4f031f39c86b5d9f6a9fd2946421
SayliDholam <saylidholam08@gmail.com>
2024-03-23 20:38:58 +0530

    Compare elements in 1D array

---
+++ Lec_5/Array_compare.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Array_compare
+{
+	public static void main(String[] args) {
+
+		int[] arr1 = {46, 85, 35, 84, 22};
+		int[] arr2 = {86,45,2,532,25};
+		int[] arr3 = {46, 85, 35, 84, 22};
+
+		System.out.println(Arrays.equals(arr1, arr2));
+		System.out.println(Arrays.equals(arr1, arr3));
+	}
+}
+
