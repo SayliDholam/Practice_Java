f89b059016fd87a86d0c8febccc810746b08aafd
SayliDholam <saylidholam08@gmail.com>
2024-07-22 01:15:06 +0530

    Time-Space complexity

---
+++ My/zothers/TimeSpace.java
@@ -0,0 +1,85 @@
+/*
+TIME COMPLEXITY :
+
+A function that gives us the relationship about how the time grows when the input grows.
+
+time complexity != time taken
+
+O(1) < o(logN) < O(N)
+
+always look for worst case complexity
+
+always look at the complexity for large/infinite data
+
+even though value of actual time is different, they are all growing 
+
+we do not care about actual time taken, we care about input-time relation
+
+constants are ignored 
+
+----------------------
+
+BIG O NOTATION : 
+
+it is the upper bound (it cannot exceed beyond this) 
+
+it is the maximum possible
+
+(less than and equal to)
+
+----------------------
+
+BIG OMEGA NOTATION : 
+
+opposite of big O notation
+
+it is the lower bound (it cannot be less than this)
+
+it is the minimum required
+
+(greater than and equal to)
+
+----------------------
+
+BIG THETA NOTATION : 
+
+it is combining both upper and lower bound
+
+big O + big omega = big theta
+
+----------------------
+
+LITTLE O NOTATION : 
+
+it also gives the upper bound but not strict 
+
+(only less than)
+
+----------------------
+
+LITTLE OMEGA NOTATION : 
+
+it also gives the lower bound but its strictly
+
+(only greater than)
+
+
+
+-------------------------------------------------------------------------------
+
+SPACE COMPLEXITY :
+
+space complexity = input size space + auxiliary space
+
+auxiliary space is the extra space or temporary space used by the algirithm
+
+
+*/
+
+class TimeSpace
+{
+	 public static void main(String[] args) 
+	 {
+	 	System.out.println("This is space & time complexity")
+	 }  
+}
