20fb9ee7dbc9b9c641a030e74c32d3a3c1dd476f
SayliDholam <saylidholam08@gmail.com>
2024-08-13 01:52:59 +0530

    inbuilt stacks

--- My/stack/Stacks.java
+++ My/stack/Stacks.java
@@ -1,10 +1,25 @@
-import java.util.*;
+//inbuilt stack
 
+
+import java.util.*;
 
 class Stacks
 {
 	public static void main(String[] args) 
 	{
-		
+		Stack<Integer> st = new Stack<>();
+
+		st.push(65);
+		st.push(97);
+		st.push(24);
+		st.push(7);
+		st.push(32);
+
+		System.out.println(st.pop());
+		System.out.println(st.pop());
+
+		System.out.println("Top element (peek): " + st.peek());
+
+		System.out.println("Is stack empty? " + st.empty());
 	}
 }
